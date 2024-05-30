aaac2d3dc2f36d894fa471e2f64607f1255016aa
Rian-Rero <rian@dharma.com.br>
2024-05-30 17:18:54 -0300

    feat: beatiful menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -4,107 +4,106 @@
 
 using namespace std;
 
+void printBoxedText(const string &text)
+{
+    string border = "╔════════════════════════════════════════╗";
+    string padding = "║                                        ║";
+
+    cout << border << endl;
+    cout << "║ " << text;
+    for (size_t i = text.size(); i < 38; ++i)
+        cout << " ";
+    cout << " ║" << endl;
+    cout << "╚════════════════════════════════════════╝" << endl;
+}
+
 void Menu::displayMainMenu_()
 {
-    cout << "==============================" << endl;
-    cout << "  Seja Bem-Vindo ao LibrarySoftware" << endl;
-    cout << "==============================" << endl;
-    cout << "1. Login" << endl;
-    cout << "2. Registrar" << endl;
-    cout << "3. Sair" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Seja Bem-Vindo ao LibrarySoftware");
+    cout << "╔════════════════════════════════════════╗" << endl;
+    cout << "║ 1. Login                               ║" << endl;
+    cout << "║ 2. Registrar                           ║" << endl;
+    cout << "║ 3. Sair                                ║" << endl;
+    cout << "╚════════════════════════════════════════╝" << endl;
     cout << "Escolha uma opção: ";
 }
 
 void Menu::displayLoggedInAdminMenu_(Users *user)
 {
-
-    cout << "==============================" << endl;
-    cout << "         Olá " << user->getName() << endl;
-    cout << "==============================" << endl;
-    cout << "1. Cadastrar Livro" << endl;
-    cout << "2. Ver Lista de Livros" << endl;
-    cout << "3. Ver Lista de Livros Disponíveis" << endl;
-    cout << "4. Ver Lista de Livros Alugados" << endl;
-    cout << "5. Ver seus Livros Alugados" << endl;
-    cout << "6. Ver histórico de aluguéis" << endl;
-    cout << "7. Acesssar histórias de usuários" << endl;
-    cout << "8. Conceder permissão de administrador" << endl;
-    cout << "9. Cadastrar um funcionário" << endl;
-    cout << "10. Alugar um Livro" << endl;
-    cout << "11. Logout" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Olá " + user->getName());
+    cout << "╔════════════════════════════════════════╗" << endl;
+    cout << "║ 1. Cadastrar Livro                     ║" << endl;
+    cout << "║ 2. Ver Lista de Livros                 ║" << endl;
+    cout << "║ 3. Ver Lista de Livros Disponíveis     ║" << endl;
+    cout << "║ 4. Ver Lista de Livros Alugados        ║" << endl;
+    cout << "║ 5. Ver seus Livros Alugados            ║" << endl;
+    cout << "║ 6. Ver histórico de aluguéis           ║" << endl;
+    cout << "║ 7. Acessar histórias de usuários       ║" << endl;
+    cout << "║ 8. Conceder permissão de administrador ║" << endl;
+    cout << "║ 9. Cadastrar um funcionário            ║" << endl;
+    cout << "║ 10. Alugar um Livro                    ║" << endl;
+    cout << "║ 11. Logout                             ║" << endl;
+    cout << "╚════════════════════════════════════════╝" << endl;
     cout << "Escolha uma opção: ";
 }
 
 void Menu::displayLoggedInMenu_(Users *user)
 {
-    cout << "==============================" << endl;
-    cout << "            Olá " << user->getName() << endl;
-    cout << "==============================" << endl;
-    cout << "1. Ver Lista de Livros" << endl;
-    cout << "2. Ver Lista de Livros Disponíveis" << endl;
-    cout << "3. Ver seus Livros Alugados" << endl;
-    cout << "4. Ver histórico de aluguéis" << endl;
-    cout << "5. Acesssar histórias de usuários" << endl;
-    cout << "6. Alugar um Livro" << endl;
-    cout << "7. Logout" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Olá " + user->getName());
+    cout << "╔════════════════════════════════════════╗" << endl;
+    cout << "║ 1. Ver Lista de Livros                 ║" << endl;
+    cout << "║ 2. Ver Lista de Livros Disponíveis     ║" << endl;
+    cout << "║ 3. Ver seus Livros Alugados            ║" << endl;
+    cout << "║ 4. Ver histórico de aluguéis           ║" << endl;
+    cout << "║ 5. Acessar histórias de usuários       ║" << endl;
+    cout << "║ 6. Alugar um Livro                     ║" << endl;
+    cout << "║ 7. Logout                              ║" << endl;
+    cout << "╚════════════════════════════════════════╝" << endl;
     cout << "Escolha uma opção: ";
 }
 
 void Menu::displayEmployeerLoggedInMenu(Users *users)
 {
-    cout << "==============================" << endl;
-    cout << "         Olá " << users->getName() << endl;
-    cout << "==============================" << endl;
-    cout << "1. Ver Lista de Livros" << endl;
-    cout << "2. Ver Lista de Livros Disponíveis" << endl;
-    cout << "3. Ver seus Livros Alugados" << endl;
-    cout << "4. Ver histórico de aluguéis" << endl;
-    cout << "5. Acesssar histórias de usuários" << endl;
-    cout << "6. Logout" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Olá " + users->getName());
+    cout << "╔════════════════════════════════════════╗" << endl;
+    cout << "║ 1. Ver Lista de Livros                 ║" << endl;
+    cout << "║ 2. Ver Lista de Livros Disponíveis     ║" << endl;
+    cout << "║ 3. Ver seus Livros Alugados            ║" << endl;
+    cout << "║ 4. Ver histórico de aluguéis           ║" << endl;
+    cout << "║ 5. Acessar histórias de usuários       ║" << endl;
+    cout << "║ 6. Logout                              ║" << endl;
+    cout << "╚════════════════════════════════════════╝" << endl;
     cout << "Escolha uma opção: ";
 }
 
 void Menu::login_()
 {
-    cout << "==============================" << endl;
-    cout << "          Faça o login" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Faça o login");
 }
 
 void Menu::registerUser_()
 {
-    cout << "==============================" << endl;
-    cout << "         Faça o registro" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Faça o registro");
 }
 
 void Menu::createBooks_()
 {
-    cout << "==============================" << endl;
-    cout << "      Cadastro de Livro" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Cadastro de Livro");
 }
 
 void Menu::closeSoftware_()
 {
-    cout << "==============================" << endl;
-    cout << "    Saindo do software" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Saindo do software");
     exit(0);
 }
 
 void Menu::displayAllBooks_()
 {
-    cout << "==============================" << endl;
-    cout << "         Pesquisar Livros" << endl;
-    cout << "==============================" << endl;
-    cout << "1. Pesquisar por nome" << endl;
-    cout << "2. Pesquisar por autor" << endl;
-    cout << "3. Voltar" << endl;
-    cout << "==============================" << endl;
+    printBoxedText("Pesquisar Livros");
+    cout << "╔════════════════════════════════════════╗" << endl;
+    cout << "║ 1. Pesquisar por nome                  ║" << endl;
+    cout << "║ 2. Pesquisar por autor                 ║" << endl;
+    cout << "║ 3. Voltar                              ║" << endl;
+    cout << "╚════════════════════════════════════════╝" << endl;
     cout << "Escolha uma opção: ";
 }
